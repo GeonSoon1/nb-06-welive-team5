3660c7b9d8dc26efd3240130a923a1f61bf67423
YooInHak <dlsgkr2856@gmail.com>
2026-03-27 10:51:37 +0900

    fix:test코드 에러 수정

--- src/polls/poll.router.ts
+++ src/polls/poll.router.ts
@@ -2,16 +2,16 @@ import { EXPRESS } from '../libs/constants';
 import * as pollController from './poll.controller';
 import { catchAsync } from '../libs/catchAsync';
 import { authenticate } from '../middlewares/authenticate';
-//추후 프론트 엔드 코드 보고 수정, 관리자만 투표들 생성이 가능한지 확인
-// import { authorize } from '../middlewares/authorize';
+import { authorize } from '../middlewares/authorize';
+import { Role } from '@prisma/client';
 const pollRouter = EXPRESS.Router();
 
-pollRouter.post('/', authenticate, catchAsync(pollController.CreatePolls));
+pollRouter.post('/', authenticate, authorize(Role.ADMIN, Role.SUPER_ADMIN), catchAsync(pollController.CreatePolls));
 
 pollRouter.get('/', authenticate, catchAsync(pollController.GetAllPollList));
 
 pollRouter.get('/:pollId', authenticate, catchAsync(pollController.GetPollInformation));
-pollRouter.patch('/:pollId', authenticate, catchAsync(pollController.UpdatePoll));
-pollRouter.delete('/:pollId', authenticate, catchAsync(pollController.DeletePoll));
+pollRouter.patch('/:pollId', authenticate, authorize(Role.ADMIN, Role.SUPER_ADMIN), catchAsync(pollController.UpdatePoll));
+pollRouter.delete('/:pollId', authenticate, authorize(Role.ADMIN, Role.SUPER_ADMIN), catchAsync(pollController.DeletePoll));
 
 export default pollRouter;
